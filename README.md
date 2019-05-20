927d3a209ade03d88bc952f145b3052ecfa098e1
Yerko De Zutter <yerkodezutter@gmail.com>
2019-05-20 09:48:56 +0200

    link canvas

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Basic Setup
+# Linking Canvas
 
 k
---
+++ index.js
@@ -0,0 +1,7 @@
+let canvas = document.querySelector("canvas");
+canvas.width = 300;
+canvas.height = 300;
+let context = canvas.getContext('2d');
+
+context.fillStyle = "#000";
+context.fillRect(0, 0, canvas.width, canvas.height);
