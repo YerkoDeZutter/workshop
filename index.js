79cf198c9fe4432e7d9bdc9c683a45afdca6e381
Yerko De Zutter <yerkodezutter@gmail.com>
2019-05-20 10:04:53 +0200

    add step_2 comments

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let canvas = document.querySelector("canvas");
-canvas.width = 300;
-canvas.height = 300;
-let context = canvas.getContext('2d');
+let canvas = document.querySelector("canvas"); // make var of canvas from html
+canvas.width = 300; // give canvas a width
+canvas.height = 300; // give canvas a height
+let context = canvas.getContext('2d'); // get context of canvas. and put it in a var
 
-context.fillStyle = "#000";
-context.fillRect(0, 0, canvas.width, canvas.height);
+context.fillStyle = "#000"; // give collor to shape (background)
+context.fillRect(0, 0, canvas.width, canvas.height);// make shape (background)
