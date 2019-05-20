1b762134ed2a3193219e221edd71629ec6873f05
Yerko De Zutter <yerkodezutter@gmail.com>
2019-05-20 09:49:28 +0200

    just full screen

--- index.js
+++ index.js
@@ -3,10 +3,5 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let context = canvas.getContext('2d');
 
-context.fillStyle = "#FFF";
+context.fillStyle = "#000";
 context.fillRect(0, 0, canvas.width, canvas.height);
-
-context.beginPath();
-context.fillStyle = 'red';
-context.arc(100, 100, 50, 0, 2 * Math.PI);
-context.stroke();
