69dd73dc5bbdfecc706376113e29bcbe484d77bb
Yerko De Zutter <yerkodezutter@gmail.com>
2019-05-20 09:57:28 +0200

    add step_4 comments

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ let context = canvas.getContext('2d');
 context.fillStyle = "#FFF";
 context.fillRect(0, 0, canvas.width, canvas.height);
 
-context.beginPath();
-context.fillStyle = 'red';
-context.arc(100, 100, 50, 0, 2 * Math.PI);
-context.stroke();
+
+
+
+// DRAW CIRCLE
+
+context.beginPath(); // begin making shape
+context.arc(100, 100, 50, 0, 2 * Math.PI); // make a circle
+context.stroke(); // end making shape
